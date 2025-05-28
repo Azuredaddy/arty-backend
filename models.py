00701e7092c705f6e7e9414c827073c3a1dc579a
Azuredaddy <141799941+Azuredaddy@users.noreply.github.com>
2025-05-28 16:49:57 +1000

    Update models.py

--- models.py
+++ models.py
@@ -1,19 +1,8 @@
-from sqlalchemy import Column, Integer, String, create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
-
-DATABASE_URL = "sqlite:///./database.db"
-
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
-SessionLocal = sessionmaker(bind=engine)
-
-Base = declarative_base()
+from sqlalchemy import Column, Integer, String
+from database import Base
 
 class User(Base):
     __tablename__ = "users"
     id = Column(Integer, primary_key=True, index=True)
     email = Column(String, unique=True, index=True)
     tenant_id = Column(String, index=True)
-
-def init_db():
-    Base.metadata.create_all(bind=engine)
